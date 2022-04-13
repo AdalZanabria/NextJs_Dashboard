0a8a4f0cc96f3591068010058b3d0b371b05ffae
AdalZanabria <adal.zanabria@gmail.com>
2022-04-12 23:15:47 -0500

    Terminado sistema de login.

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,4 +1,5 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
+import { useRouter } from 'next/router';
 import { LockClosedIcon } from '@heroicons/react/solid';
 import { useAuth } from '@hooks/useAuth';
 
@@ -6,15 +7,33 @@ export default function LoginPage() {
   const emailRef = useRef(null);
   const passwordRef = useRef(null);
   const auth = useAuth();
+  const router = useRouter();
+
+  const [errorLogin, setErrorLogin] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   const submitHandler = (event) => {
     event.preventDefault();
     const email = emailRef.current.value;
     const password = passwordRef.current.value;
+    setErrorLogin(null);
+    setLoading(true);
 
-    auth.signIn(email, password).then(() => {
-      console.log('Login success');
-    });
+    auth
+      .signIn(email, password)
+      .then(() => {
+        router.push('/dashboard');
+      })
+      .catch(function (error) {
+        if (error.response?.status === 401) {
+          setErrorLogin('Usuario o password incorrecto.');
+        } else if (error.request) {
+          setErrorLogin('Tenemos un problema');
+        } else {
+          setErrorLogin('Algo salió mal.');
+        }
+        setLoading(false);
+      });
   };
 
   return (
@@ -59,6 +78,11 @@ export default function LoginPage() {
                 />
               </div>
             </div>
+            {errorLogin && (
+              <div className="p-3 my-3 text-sm text-red-700 bg-red-100 rounded-lg dark:bg-red-200 dark:text-red-800" role="alert">
+                <span className="font-medium">Error!</span> {errorLogin}
+              </div>
+            )}
 
             <div className="flex items-center justify-between">
               <div className="flex items-center">
@@ -81,6 +105,12 @@ export default function LoginPage() {
                 className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
               >
                 <span className="absolute left-0 inset-y-0 flex items-center pl-3">
+                  {loading && (
+                    <span className="flex absolute h-4 w-4 top-0 right-0 -mt-1 -mr-1">
+                      <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-indigo-300 opacity-75"></span>
+                      <span className="relative inline-flex rounded-full h-4 w-4 bg-indigo-400"></span>
+                    </span>
+                  )}
                   <LockClosedIcon className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" aria-hidden="true" />
                 </span>
                 Sign in

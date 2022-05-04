9249c5e07a1735d8b77b8138c37abd97a703bc1e
AdalZanabria <adal.zanabria@gmail.com>
2022-05-03 19:01:27 -0500

    Agregada funcionalidad de logout.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,7 +12,6 @@ const navigation = [
 const userNavigation = [
   { name: 'Your Profile', href: '#' },
   { name: 'Settings', href: '#' },
-  { name: 'Sign out', href: '#' },
 ];
 
 function classNames(...classes) {
@@ -82,15 +81,9 @@ export default function Header() {
                         leaveTo="transform opacity-0 scale-95"
                       >
                         <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-                          {userNavigation.map((item) => (
-                            <Menu.Item key={item.name}>
-                              {({ active }) => (
-                                <a href={item.href} className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}>
-                                  {item.name}
-                                </a>
-                              )}
-                            </Menu.Item>
-                          ))}
+                          <button onClick={() => auth.logout()} className="block px-4 py-2 text-sm text-gray-700">
+                            Logout
+                          </button>
                         </Menu.Items>
                       </Transition>
                     </Menu>
--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -38,8 +38,16 @@ function useProviderAuth() {
     }
   };
 
+  const logout = () => {
+    Cookie.remove('token');
+    setUser(null);
+    delete axios.defaults.headers.Authorization;
+    window.location.href = '/login';
+  };
+
   return {
     user,
     signIn,
+    logout,
   };
 }
